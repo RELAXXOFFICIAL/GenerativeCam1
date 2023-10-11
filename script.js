afe6035f1d9ee80010cce82e77985484cf805069
RELAXXOFFICIAL <128732812+RELAXXOFFICIAL@users.noreply.github.com>
2023-10-11 02:46:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
-// Get references to the canvas and context
-const canvas = document.getElementById('canvas');
+// Get the canvas element
+const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-// Set up the audio context
+// Create a new audio context
 const audioCtx = new AudioContext();
+
+// Create a buffer source node
 const source = audioCtx.createBufferSource();
+
+// Connect the source to the destination
 source.connect(audioCtx.destination);
+
+// Start the source
 source.start();
 
 // Define the draw function
 function draw() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Get the current time
+  const now = audioCtx.currentTime;
 
-    // Get the current time from the audio context
-    const now = audioCtx.currentTime;
+  // Calculate the frequency
+  const frequency = source.frequency;
 
-    // Calculate the frequency and amplitude of the audio signal
-    const frequency = source.frequency;
-    const amplitude = source.amplitude;
+  // Calculate the amplitude
+  const amplitude = source.amplitude;
 
-    // Draw a line based on the frequency and amplitude
-    ctx.beginPath();
-    ctx.strokeStyle = `hsl(${frequency}, 100%, 50%)`;
-    ctx.lineWidth = amplitude;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(canvas.width, canvas.height);
-    ctx.stroke();
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Set the stroke style
+  ctx.strokeStyle = `hsl(${frequency}, 100%, 50%)`;
+
+  // Set the line width
+  ctx.lineWidth = amplitude;
+
+  // Draw a line from the top left to the bottom right
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(canvas.width, canvas.height);
+  ctx.stroke();
+
+  // Schedule the next frame
+  requestAnimationFrame(draw);
 }
 
-// Call the draw function repeatedly
-setInterval(draw, 1000 / 60);
+// Start the animation loop
+requestAnimationFrame(draw);
+
+// Clean up
+return () => {
+  // Stop the source
+  source.stop();
+
+  // Close the audio context
+  audioCtx.close();
+
+  // Cancel the animation loop
+  cancelAnimationFrame(requestId);
+};
